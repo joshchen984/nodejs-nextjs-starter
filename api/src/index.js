0864ba003771b76eb1d47ce3358ce8c515bfbeff
Josh Chen <joshchen984@gmail.com>
2021-04-07 21:39:14 -0700

    Connect frontend and backend

--- api/src/index.js
+++ api/src/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 
 const app = express();
+const port = 3001;
 
 app.get('/', (req, res) => {
   res.send('Hello');
 });
 
-app.listen(3000, () => {
-  console.log('Listening at http://localhost:3000');
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
 });
--- client/next.config.js
+++ client/next.config.js
@@ -1,8 +1,16 @@
 module.exports = {
   webpack: (config) => {
     config.node = {
-      fs: 'empty'
-    }
-    return config
-  }
+      fs: 'empty',
+    };
+    return config;
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api',
+        destination: 'http://localhost:3001', // Proxy to Backend
+      },
+    ];
+  },
 };
