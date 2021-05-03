3fbc91b748df26a43fadecceffadf379b1e7fb7e
joshchen984 <joshchen984@gmail.com>
2021-05-02 20:33:12 -0700

    get rid of eslint errors in _app.tsx

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,8 +1,9 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import '../styles/globals.scss';
 import React from 'react';
+import { AppProps } from 'next/app';
 
-function Application({ Component, pageProps }): JSX.Element {
+function Application({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
 
