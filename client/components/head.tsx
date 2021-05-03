883208a1f584663311b535ce0ce117871d017d18
joshchen984 <joshchen984@gmail.com>
2021-05-02 20:19:35 -0700

    get rid of eslint errors in head component

--- client/components/head.tsx
+++ client/components/head.tsx
@@ -2,21 +2,20 @@ import React from 'react';
 import NextHead from 'next/head';
 import { string } from 'prop-types';
 
-const defaultDescription = '';
-const defaultKeywords = '';
-const defaultOGURL = '';
-const defaultOGImage = '';
-
-const Head = (props) => (
+interface HeadProps {
+  title: string;
+  description: string;
+  keywords: string;
+  url: string;
+  ogImage: string;
+}
+const Head = ({ title, description, keywords, url, ogImage }: HeadProps) => (
   <NextHead>
     <meta charSet="UTF-8" />
-    <title>{props.title || ''}</title>
+    <title>{title}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta
-      name="description"
-      content={props.description || defaultDescription}
-    />
-    <meta name="keywords" content={props.keywords || defaultKeywords} />
+    <meta name="description" content={description} />
+    <meta name="keywords" content={keywords} />
     <link
       rel="icon"
       type="image/png"
@@ -36,16 +35,13 @@ const Head = (props) => (
       href="/static/apple-touch-icon.png"
     />
     <link rel="mask-icon" href="/static/favicon-mask.svg" color="#000000" />
-    <meta property="og:url" content={props.url || defaultOGURL} />
-    <meta property="og:title" content={props.title || ''} />
-    <meta
-      property="og:description"
-      content={props.description || defaultDescription}
-    />
-    <meta name="twitter:site" content={props.url || defaultOGURL} />
+    <meta property="og:url" content={url} />
+    <meta property="og:title" content={title} />
+    <meta property="og:description" content={description} />
+    <meta name="twitter:site" content={url} />
     <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:image" content={props.ogImage || defaultOGImage} />
-    <meta property="og:image" content={props.ogImage || defaultOGImage} />
+    <meta name="twitter:image" content={ogImage} />
+    <meta property="og:image" content={ogImage} />
     <meta property="og:image:width" content="1200" />
     <meta property="og:image:height" content="630" />
   </NextHead>
@@ -57,6 +53,13 @@ Head.propTypes = {
   keywords: string,
   url: string,
   ogImage: string,
+};
+Head.defaultProps = {
+  title: '',
+  description: '',
+  keywords: '',
+  ogImage: '',
+  url: '',
 };
 
 export default Head;
